05326f4e4a0ccd5b62308645364d07d99b392cf7
apyd <arturpydych@gmail.com>
2019-07-28 07:18:16 +0200

    User selection appears in appropriate box
    - User selection appears in appropriate box
    - By default question mark icon appears within box (before any user selection)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
 var modals = document.getElementsByClassName('modal-bg');
 var links = document.getElementsByClassName('nav__link');
 var closeButtons = document.getElementsByClassName("modal--close")
+var choiceOptions = document.getElementsByClassName('choice-container__icon');
+var userChoice;
 var openedModal;
+
 
 for(var i=0; i < links.length; i++) {
     links[i].addEventListener("click", showModal);
     links[i].elementNumber = i;
     closeButtons[i].addEventListener("click", closeModal);
-    window.addEventListener("click", closedModal2);
+    window.addEventListener("click", closeModalWhenClickOutside);
 }
 
-function showModal() {
+function showModal(e) {
     if(openedModal != null) {
         closeModal();
 }
-    openedModal = event.srcElement.elementNumber;    
+    openedModal = e.srcElement.elementNumber;    
     modals[openedModal].style.visibility = 'visible';
 };
 
@@ -22,11 +25,24 @@ function closeModal() {
     modals[openedModal].style.visibility = 'hidden';
 };
 
-function closedModal2() {
-    if(event.target == modals[openedModal]) {
+function closeModalWhenClickOutside(e) {
+    if(e.target == modals[openedModal]) {
     for(i = 0; i < modals.length; i++) {
         modals[i].style.visibility = "hidden";
     }
 }
+}
+
+var choiceOptions = document.getElementsByClassName('choice-container__icon');
+for(var i=0; i < choiceOptions.length; i++) {
+    choiceOptions[i].addEventListener("click", selectedOption);
+    choiceOptions[i].optionNumber = i;
+}
+
+function selectedOption(e) {
+    userChoice = choiceOptions[e.currentTarget.optionNumber];
+    var imgPath = userChoice.children[0].src;
+    var userSelection = document.getElementsByClassName('user-choice__icon');
+    userSelection[0].src = imgPath;
 }
 
