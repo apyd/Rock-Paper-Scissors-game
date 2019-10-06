37281b133f8659c9f17119cfa34fea6f861b5520
apyd <arturpydych@gmail.com>
2019-10-06 20:35:44 +0200

    Storing user and pc randomly generated selection
    - opponent selection is generated randomly
    - storing opponent selection and adding appropriate src to img element
    - storing user selection data-value

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var choiceOptions = document.getElementsByClassName("choice-container__icon");
 var gameOptions = document.getElementsByClassName("game-options__item");
 var startGameBtn = document.getElementsByClassName("btn");
 var formBg = document.getElementsByClassName("form-bg");
-var openedModalBg, activeNavItem, selectedType;
+var openedModalBg, activeNavItem, selectedGameType;
 
 for (var i = 0; i < navLinks.length; i++) {
   navLinks[i].addEventListener("click", onNavItemClick);
@@ -21,14 +21,15 @@ startGameBtn[0].addEventListener("click", getUserName);
 startGameBtn[0].addEventListener("click", startGame);
 
 function startGame() {
-  if (!selectedType || !model.username) {
+  if (!selectedGameType || !model.username) {
     return alert(
       "If you want to start a game please specify your name and select game type"
     );
   }
-  model.gameType = selectedType.dataset.value;
+  model.gameType = selectedGameType.dataset.value;
   view.displayUsername();
   view.displayGameType(model.gameType);
+  model.generateOpponentSelection();
   formBg[0].style.visibility = "hidden";
 }
 
@@ -37,20 +38,20 @@ function getUserName() {
 }
 
 function onGameTypeClick(e) {
-  if (selectedType != null) {
+  if (selectedGameType != null) {
     unselectGameType();
   }
   selectGameType(e);
 }
 
 function unselectGameType() {
-  selectedType.classList.remove("game-options__item--active");
-  selectedType = null;
+  selectedGameType.classList.remove("game-options__item--active");
+  selectedGameType = null;
 }
 
 function selectGameType(e) {
-  selectedType = e.currentTarget;
-  selectedType.classList.add("game-options__item--active");
+  selectedGameType = e.currentTarget;
+  selectedGameType.classList.add("game-options__item--active");
 }
 
 function onNavItemClick(e) {
@@ -99,16 +100,17 @@ for (var i = 0; i < choiceOptions.length; i++) {
 }
 
 function selectedOption(e) {
-  var userChoice = e.currentTarget.children[0].src;
-  document.getElementsByClassName("user-choice__icon")[0].src = userChoice;
+  model.userChoice = e.currentTarget.children[0].dataset.value;
+  var userChoiceUrl = e.currentTarget.children[0].src;
+  document.getElementsByClassName("user-choice__icon")[0].src = userChoiceUrl;
 }
 
 let view = {
-  displayUsername: function () {
-    var elementWithUsername = document.getElementsByClassName("jsUsername");
-    elementWithUsername[0].innerHTML = model.username + '\'s' + ' score';
+  displayUsername: function() {
+    var elementWithUsername = document.getElementsByClassName("jsUsername")[0];
+    elementWithUsername.innerHTML = model.username + '\'s' + ' score';
   },
-  displayGameType: function (starsNumber) {
+  displayGameType: function(starsNumber) {
     var scoreBoxes = document.getElementsByClassName("score-box");
     for (var j = 0; j < scoreBoxes.length; j++) {
       for (var i = 0; i < starsNumber; i++) {
@@ -119,12 +121,29 @@ let view = {
         scoreBoxes[j].appendChild(starIcon);
       }
     }
+  },
+  displayMessage: function(message) {
+	var messageBox = document.getElementsByClassName("message-box__text")[0];
+	messageBox.innerHTML = message;
   }
 };
 
-let controller = {};
+let controller = {
+
+};
 
 let model = {
   username: null,
-  gameType: null
+  gameType: null,
+  userScore: 0,
+  opponentScore: 0,
+  userChoice: null,
+  opponentChoice:  null,
+  availableSelections : [["p","img/paper-96.png"],["r","img/rock-96.png"],["s","img/scissors-96.png"]], // p - paper, r - rock, s - scissors
+  generateOpponentSelection : function() {
+    var selectionArray = this.availableSelections[Math.floor(Math.random() * 3)];
+    this.opponentChoice = selectionArray[0];
+    var opponentChoiceIcon = document.getElementsByClassName("opponent-choice__icon")[0];
+    opponentChoiceIcon.src = selectionArray[1];
+  }
 };
